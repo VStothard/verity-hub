2f633a242f9d2b1b7396d7af9289a38151d75abd
Verity Stothard <vjstothard@gmail.com>
2019-04-01 18:10:59 +1100

    index layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,53 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        verity-hub
-      </h1>
-      <h2 class="subtitle">
-        My classy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+  <section class="block md:flex flex-col bg-blue">
+    <!--intro page, arrow pointing up to the nav items-->
+    <!--todo: rebuild this layout with CSS grid-->
+    <div class="flex justify-end">
+      <nav>
+        <nuxt-link
+          to="/portfolio"
+          class="py-2 sm:py-4 px-2 inline-block no-underline hover:text-white"
+        >portfolio</nuxt-link>
+        <nuxt-link
+          to="/blog"
+          class="py-2 sm:py-4 px-2 inline-block no-underline hover:text-white"
+        >blog</nuxt-link>
+        <nuxt-link
+          to="/about"
+          class="py-2 sm:py-4 px-2 inline-block no-underline hover:text-white"
+        >about</nuxt-link>
+      </nav>
+    </div>
+    <div class="block md:flex fill-page">
+      <div class="border flex-grow md:max-w-xs bg-teal-light">
+        <!-- intro + quick links -->
+        <div class="inline-block md:flex flex-col text-center">
+          <div class="border hidden md:block">DP HERE</div>
+          <div class="border">Some intro text here</div>
+          <div class="border hidden md:block">Social links</div>
+        </div>
       </div>
+      <div class="border flex-grow bg-teal">body</div>
     </div>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
+.fill-page {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.link-base {
+  @apply py-4 px-2 inline-block no-underline;
 }
 </style>
+
+<script>
+export default {
+  computed: {
+    linkBase() {
+      return `border border-red`;
+    }
+  }
+};
+</script>
