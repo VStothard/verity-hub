d464cbb214f4e41067f1bc8c932169e60cff3d3e
Verity Stothard <vjstothard@gmail.com>
2019-04-10 19:21:38 +1000

    default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,32 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="w-100 min-h-screen flex flex-col">
+    <nav class="p-4 fixed w-screen bg-white z-10">
+      <div class="flex justify-end">
+        <div>
+          <nuxt-link to="/portfolio" class="p-4 link-base">Portfolio</nuxt-link>
+        </div>
+        <div>
+          <nuxt-link to="/blog" class="p-4 link-base">Blog</nuxt-link>
+        </div>
+        <div>
+          <nuxt-link to="/courses" class="p-4 link-base">Courses</nuxt-link>
+        </div>
+      </div>
+    </nav>
+    <div class="lg:flex w-100 flex-grow nav-margin">
+      <div class="w-full lg:w-1/4 relative">
+        <side-nav/>
+      </div>
+      <div class="w-full lg:w-3/4 p-2">
+        <div class="shadow-md rounded lg:mr-8">
+          <nuxt/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.nav-margin {
+  margin-top: 52px;
 }
 </style>
