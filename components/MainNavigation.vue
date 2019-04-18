bc13ed2aae69f866d6eed78d12ca31dd9601c55e
Verity Stothard <vjstothard@gmail.com>
2019-04-18 15:34:08 +1000

    navigation fix

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -2,10 +2,10 @@
   <nav class="p-4 fixed w-screen bg-white z-10">
     <div class="flex justify-end">
       <div>
-        <nuxt-link to="/blog" class="p-4 link-base">Blog</nuxt-link>
+        <nuxt-link to="/" class="p-4 link-base">Blog</nuxt-link>
       </div>
       <div>
-        <nuxt-link to="/" class="p-4 link-base">Blog</nuxt-link>
+        <nuxt-link to="/course" class="p-4 link-base">Courses</nuxt-link>
       </div>
       <div>
         <nuxt-link to="/portfolio" class="p-4 link-base">Portfolio</nuxt-link>
