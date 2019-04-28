4c17108add97722abaee808706ef9a987f641b01
Verity Stothard <vjstothard@gmail.com>
2019-04-29 09:42:22 +1000

    remove extra access token

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,6 @@ export default {
   },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
   },
   generate: {
