b40d896ca3277760f71e41616ed335648d0ed993
Verity Stothard <vjstothard@gmail.com>
2019-05-03 13:24:39 +1000

    target blank

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -4,7 +4,7 @@
             <a class="px-4" href="https://github.com/VStothard/" target="_blank"><img src="@/assets/img/github.svg" alt="Verity Stothard Github" class="social-icon"></a>
             <a class="px-4" href="https://twitter.com/VerityStothard/" target="_blank"><img src="@/assets/img/twitter.svg" alt="Verity Stothard Twitter" class="social-icon"></a>
             <a class="px-4" href="https://www.instagram.com/veritystothard/" target="_blank"><img src="@/assets/img/instagram.svg" alt="Verity Stothard Instagram" class="social-icon"></a>
-            <a class="px-4" href="https://dev.to/vstothard"><img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Verity Stothard's DEV Profile" height="25" width="25" /></a>
+            <a class="px-4" href="https://dev.to/vstothard" target="_blank"><img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Verity Stothard's DEV Profile" height="25" width="25" /></a>
         </div>
     </div>
 </template>
